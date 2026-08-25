91ede65ef31f710a48b1c293edefbdb5bc6ef6fb
Jelmuu <hoffrenjelmer@gmail.com>
2026-08-25 14:08:06 +0300

    Add files via upload

---
+++ js/application.js
@@ -0,0 +1,11 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  window.gameManager = new GameManager(
+    4,
+    KeyboardInputManager,
+    HTMLActuator,
+    LocalStorageManager
+  );
+
+  console.log(window.gameManager);
+});
